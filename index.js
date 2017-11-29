747a38d2b48663fced3f6aabe2de6df257087f99
Ivan <plotnikov.msk@yandex.ru>
2017-11-29 14:35:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function  happyHolidaysTo(name){
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(holiday, parseInt(days)){
-  `It's ${days} days until ${holiday}!`
+function holidayCountdown(holiday, days){
+  `It's ${parseInt(days)} days until ${holiday}!`
 }
